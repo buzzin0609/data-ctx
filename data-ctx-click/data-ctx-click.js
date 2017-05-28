1c2224757e5c68144bc5a28e77562fd158c92049
William Busby <will0609@hotmail.com>
2017-05-28 01:20:22 +0100

    new detect for touch

--- data-ctx-click/data-ctx-click.js
+++ data-ctx-click/data-ctx-click.js
@@ -8,11 +8,32 @@ import handleCtxBind from '../data-ctx-bind/data-ctx-bind';
  *   Add one global click handler to the body to handle elements with data-ctx-click as a data attribute
  *   Use .closest polyfill if needed
  */
-document.addEventListener('click', function(e) {
+
+if ('ontouchstart' in window) {
+	var newTouch = false;
+
+	document.addEventListener('touchstart', function (e) {
+		newTouch = true;
+	});
+
+	document.addEventListener('touchmove', function (e) {
+		newTouch = false;
+	});
+
+	document.addEventListener('touchend', function (e) {
+		if (newTouch) {
+			decide(e);
+		}
+	});
+} else {
+	document.addEventListener('click', decide);
+}
+
+function decide(e) {
 	if (e.target.closest('[data-ctx-click]')) {
 		handleCtxClick(e);
 	}
-});
+}
 
 /**
  * Function here essentially uses the global handleHnBind function, passing in the element (referenced as this in the function context) and 'data-ctx-click' as the attribute name.
